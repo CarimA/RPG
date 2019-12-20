802a2463b8f59f3cad491d84df65dfa21bd3d47b
Carim A <CarimA@users.noreply.github.com>
2019-12-20 13:20:15 +0000

    GameTime Elapsed Seconds extension

--- PhotoVs/PhotoVs.Logic/Scenes/TextInputScene.cs
+++ PhotoVs/PhotoVs.Logic/Scenes/TextInputScene.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using PhotoVs.Models.FSM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,60 @@ using System.Threading.Tasks;
 
 namespace PhotoVs.Logic.Scenes
 {
-    class TextInputScene
+    public class TextInputScene : IUpdateableScene, IDrawableScene
     {
+        private SceneMachine _scene;
+
+        public bool IsBlocking { get; set; } = false;
+
+        private char[][] _keyboard =
+        {
+            "ABCDEFGHIJKLM".ToCharArray(),
+            "NOPQRSTUVWXYZ".ToCharArray(),
+            "abcdefghijklm".ToCharArray(),
+            "nopqrstuvwxyz".ToCharArray(),
+            "0123456789-. ".ToCharArray(),
+        };
+
+        public TextInputScene(SceneMachine scene)
+        {
+            _scene = scene;
+        }
+
+        public void Enter(params object[] args)
+        {
+            var question = args[0].ToString();
+            var limit = args.Length > 1 
+                ? int.Parse(args[1].ToString()) 
+                : 15;
+            var defaultText = args.Length > 2
+                ? args[2].ToString()
+                : string.Empty;
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        public void Resume()
+        {
+
+        }
+
+        public void Suspend()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+
+        }
     }
 }
---
+++ PhotoVs/PhotoVs.Utils/Extensions/GameTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoVs.Utils.Extensions
+{
+    public static class GameTimeExtensions
+    {
+        public static float GetElapsedSeconds(this GameTime gameTime)
+        {
+            return (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
