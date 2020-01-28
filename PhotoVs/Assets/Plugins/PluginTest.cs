7f14d2bd45d94071670a0df934cea9e5ac1d2630
Carim A <CarimA@users.noreply.github.com>
2020-01-28 16:45:30 +0000

    LiteDB Stream Provider

--- PhotoVs/Assets/Plugins/PluginTest.cs
+++ PhotoVs/Assets/Plugins/PluginTest.cs
@@ -1,9 +1,9 @@
-﻿public class PluginTest : Plugin
+﻿public class PluginTest : IPlugin
 {
     private readonly SceneMachine _sceneMachine;
 
-    public override string Name => "Test";
-    public override int Version => 1;
+    public string Name => "Test";
+    public int Version => 1;
 
     public PluginTest(Services services)
     {
@@ -20,33 +20,34 @@
 }
 
 
-public class TestPlugin : Plugin
+public class TestPlugin : IPlugin
 {
+    private Scheduler _scheduler;
     private SCamera _camera;
     private TextDatabase _db;
     private Player _player;
 
-    public override string Name => "Test2";
-    public override int Version => 55;
+    public string Name => "Test2";
+    public int Version => 55;
 
     public TestPlugin(Services services)
     {
         var events = services.Get<Events>();
-
-        events.OnInteractEventEnter["example_event"] += InteractEventHandler;
-
+        _scheduler = services.Get<Scheduler>();
         _db = services.Get<TextDatabase>();
         _camera = services.Get<SCamera>();
         _player = services.Get<Player>();
+
+        events.OnInteractEventEnter["example_event"] += InteractEventHandler;
     }
 
     private void InteractEventHandler(object sender, IGameObject player, IGameObject script)
     {
-        Spawn(LockMovement(DoThis));
+        _scheduler.Spawn(_scheduler.LockMovement(DoThis));
     }
 
     private IEnumerator DoThis()
     {
-        yield return Dialogue("Test Name", "I don't really have much to say.\nHere's a line.\nAnd another line.\nAnd another one?!");
+        yield return _scheduler.Dialogue("Test Name", "I don't really have much to say.\nHere's a line.\nAnd another line.\nAnd another one?!");
     }
 }
---
+++ PhotoVs/PhotoVs.Engine/Assets/StreamProviders/LiteDBStreamProvider.cs
@@ -0,0 +1,43 @@
+﻿using LiteDB;
+using PhotoVs.Models.Assets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PhotoVs.Engine.Assets.StreamProviders
+{
+    class LiteDBStreamProvider : IStreamProvider, IDisposable
+    {
+        private readonly LiteDatabase _database;
+
+        public string RootDirectory { get; set; }
+
+        public LiteDBStreamProvider(string filepath)
+        {
+            _database = new LiteDatabase(filepath);
+        }
+
+        public IEnumerable<string> GetDirectories(string directory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetFile(string filepath)
+        {
+            return _database.FileStorage.OpenRead(filepath);
+        }
+
+        public IEnumerable<string> GetFiles(string directory)
+        {
+            return _database.FileStorage.Find(directory)
+                .Select(file => file.Id);
+        }
+
+        public void Dispose()
+        {
+            _database.Dispose();
+        }
+    }
+}
