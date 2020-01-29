b18d5e0756c95ddee6a68c1102ad9b6df0cf6ee6
Carim A <CarimA@users.noreply.github.com>
2020-01-29 17:56:05 +0000

    Change compile shader to relative path

--- compile_shader.bat
+++ compile_shader.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\MSBuild\MonoGame\v3.0\Tools\2mgfx.exe" "E:\Projects\PhotoVsRPG\PhotoVs\Assets\Shaders\color.fx" "E:\Projects\PhotoVsRPG\PhotoVs\Assets\Shaders\color.ogl" /Profile:OpenGL
+"C:\Program Files (x86)\MSBuild\MonoGame\v3.0\Tools\2mgfx.exe" "PhotoVs\Assets\Shaders\color.fx" "PhotoVs\Assets\Shaders\color.ogl" /Profile:OpenGL
 pause
