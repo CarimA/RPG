bc1733a2022649d9afa019722b3367d516b87e7e
CarimA <akeju.carim+github@gmail.com>
2020-02-03 09:14:15 +0000

    WinDX Fullscreen Override Off

--- PhotoVs/PhotoVs.Platform.WindowsDX/WindowsDXPlatform.cs
+++ PhotoVs/PhotoVs.Platform.WindowsDX/WindowsDXPlatform.cs
@@ -6,6 +6,7 @@ namespace PhotoVs.Platform.WindowsDX
 {
     public class WindowsDXPlatform : IPlatform
     {
+        public bool OverrideFullscreen => false;
         public string PaletteShader => "shaders/color.dx11";
         public IStreamProvider StreamProvider { get; set; }
 
