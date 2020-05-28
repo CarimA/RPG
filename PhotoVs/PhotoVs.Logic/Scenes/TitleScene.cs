d084cc08d6cd22320b4ca1b9fc18e7416f835b2f
Carim A <CarimA@users.noreply.github.com>
2020-05-28 17:20:36 +0100

    Title Scene

---
+++ PhotoVs/PhotoVs.Logic/Scenes/TitleScene.cs
@@ -0,0 +1,73 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PhotoVs.Engine.Assets.AssetLoaders;
+using PhotoVs.Engine.FSM.Scenes;
+using PhotoVs.Logic.Mechanics.Input;
+using PhotoVs.Logic.PlayerData;
+using PhotoVs.Logic.Text;
+using PhotoVs.Utils.Extensions;
+using System.Collections.Generic;
+
+namespace PhotoVs.Logic.Scenes
+{
+	public class TitleScene : IUpdateableScene, IDrawableScene
+	{
+		private readonly SceneMachine _sceneMachine;
+		
+		private readonly IAssetLoader _assetLoader;
+		private readonly SpriteBatch _spriteBatch;
+		private readonly TextDatabase _textDatabase;
+
+		public bool IsBlocking { get; set; } = false;
+
+		public TitleScene(SceneMachine sceneMachine)
+		{
+			_sceneMachine = sceneMachine;
+				
+			_assetLoader = sceneMachine.Services.Get<IAssetLoader>();
+			_spriteBatch = sceneMachine.Services.Get<SpriteBatch>();
+			_textDatabase = sceneMachine.Services.Get<TextDatabase>();
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			
+		}
+
+		public void Enter(params object[] args)
+		{
+			
+		}
+
+		public void Exit()
+		{
+			
+		}
+
+		public void Resume()
+		{
+			
+		}
+
+		public void Suspend()
+		{
+			
+		}
+
+		public void Draw(GameTime gameTime)
+		{
+		}
+
+		public void DrawUI(GameTime gameTime, Matrix uiOrigin)
+		{
+			var font = _textDatabase.GetFont();
+
+			_spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive, SamplerState.PointClamp, transformMatrix: uiOrigin);
+
+      _spriteBatch.DrawStringCenterTopAligned(font, "Title Screen", new Vector2(160, 10), Color.White);
+            
+      _spriteBatch.End();
+			
+		}
+	}
+}
