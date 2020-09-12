801ea5d82a22446934ba734ae198b8303f6b4e97
Carim A <carim+github@glaciate.net>
2020-09-13 00:35:16 +0100

    Master map

---
+++ PhotoVs/devdata/GameMaps/Tilesets/Cliffs-64x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Cliffs-64x" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="Cliffs-64x.png" width="256" height="256"/>
+</tileset>
---
+++ PhotoVs/devdata/GameMaps/Tilesets/Terrain.tsx
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Terrain" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="Terrain.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="13"/>
+  <terrain name="Grass" tile="0"/>
+  <terrain name="Mid Grass" tile="4"/>
+  <terrain name="Dark Grass" tile="8"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,1"/>
+ <tile id="4" terrain="2,2,2,2"/>
+ <tile id="8" terrain="3,3,3,3"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="16" terrain=",,,1"/>
+ <tile id="17" terrain=",,1,1"/>
+ <tile id="18" terrain=",,1,1"/>
+ <tile id="19" terrain=",,1,"/>
+ <tile id="20" terrain=",,,2"/>
+ <tile id="21" terrain=",,2,2"/>
+ <tile id="22" terrain=",,2,2"/>
+ <tile id="23" terrain=",,2,"/>
+ <tile id="24" terrain=",,,3"/>
+ <tile id="25" terrain=",,3,3"/>
+ <tile id="26" terrain=",,3,3"/>
+ <tile id="27" terrain=",,3,"/>
+ <tile id="28" terrain=",,,0"/>
+ <tile id="29" terrain=",,0,0"/>
+ <tile id="30" terrain=",,0,0"/>
+ <tile id="31" terrain=",,0,"/>
+ <tile id="32" terrain=",1,,1"/>
+ <tile id="33" terrain=",1,1,1"/>
+ <tile id="34" terrain="1,,1,1"/>
+ <tile id="35" terrain="1,,1,"/>
+ <tile id="36" terrain=",2,,2"/>
+ <tile id="37" terrain=",2,2,2"/>
+ <tile id="38" terrain="2,,2,2"/>
+ <tile id="39" terrain="2,,2,"/>
+ <tile id="40" terrain=",3,,3"/>
+ <tile id="41" terrain=",3,3,3"/>
+ <tile id="42" terrain="3,,3,3"/>
+ <tile id="43" terrain="3,,3,"/>
+ <tile id="44" terrain=",0,,0"/>
+ <tile id="45" terrain=",0,0,0"/>
+ <tile id="46" terrain="0,,0,0"/>
+ <tile id="47" terrain="0,,0,"/>
+ <tile id="48" terrain=",1,,1"/>
+ <tile id="49" terrain="1,1,,1"/>
+ <tile id="50" terrain="1,1,1,"/>
+ <tile id="51" terrain="1,,1,"/>
+ <tile id="52" terrain=",2,,2"/>
+ <tile id="53" terrain="2,2,,2"/>
+ <tile id="54" terrain="2,2,2,"/>
+ <tile id="55" terrain="2,,2,"/>
+ <tile id="56" terrain=",3,,3"/>
+ <tile id="57" terrain="3,3,,3"/>
+ <tile id="58" terrain="3,3,3,"/>
+ <tile id="59" terrain="3,,3,"/>
+ <tile id="60" terrain=",0,,0"/>
+ <tile id="61" terrain="0,0,,0"/>
+ <tile id="62" terrain="0,0,0,"/>
+ <tile id="63" terrain="0,,0,"/>
+ <tile id="64" terrain=",1,,"/>
+ <tile id="65" terrain="1,1,,"/>
+ <tile id="66" terrain="1,1,,"/>
+ <tile id="67" terrain="1,,,"/>
+ <tile id="68" terrain=",2,,"/>
+ <tile id="69" terrain="2,2,,"/>
+ <tile id="70" terrain="2,2,,"/>
+ <tile id="71" terrain="2,,,"/>
+ <tile id="72" terrain=",3,,"/>
+ <tile id="73" terrain="3,3,,"/>
+ <tile id="74" terrain="3,3,,"/>
+ <tile id="75" terrain="3,,,"/>
+ <tile id="76" terrain=",0,,"/>
+ <tile id="77" terrain="0,0,,"/>
+ <tile id="78" terrain="0,0,,"/>
+ <tile id="79" terrain="0,,,"/>
+</tileset>
